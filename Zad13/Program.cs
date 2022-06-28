c0d3f5633a07ed2ad138df7a7325551a60d4d691
Vladislav <kaygorodov.vlad@gmail.com>
2022-06-28 21:20:01 +0700

    Решил задачу 13

---
+++ Zad13/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = new Random().Next(10,1000);
+Console.WriteLine($"Рандомное число: {number}");
+
+if ( number / 100 == 0)
+{
+    Console.WriteLine("Ошибка, число не являтся трехзначным");
+}
+else
+{
+    int FindNum = number % 10;
+    Console.WriteLine($"Третьим числом заданного значения является: {FindNum}");
+}
