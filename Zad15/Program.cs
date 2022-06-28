ff97650b5913245fd2d6d3c22a2ca01db0728f35
Vladislav <kaygorodov.vlad@gmail.com>
2022-06-28 21:46:59 +0700

    Выполнил задание 15

---
+++ Zad15/Program.cs
@@ -0,0 +1,42 @@
+﻿int number = new Random().Next(1,8);
+Console.WriteLine($"Номер дня недели: {number}");
+
+for (;number < 5;)
+{
+    Console.WriteLine("Выбранное число является буднем днем: ");
+    if (number == 1)
+    {
+    Console.Write("понедельник");
+    }
+   if (number == 2)
+    {
+    Console.Write("вторник");
+    }
+    if (number == 3)
+    {
+    Console.Write("среда");
+    }
+    if (number == 4)
+    {
+    Console.Write("четверг");
+    }
+    if (number == 5)
+    {
+    Console.Write("пятница");
+    }
+    break;
+}
+
+for (;number >5;)
+{
+    Console.WriteLine("Выбранное число является выходным днем: ");
+    if (number == 6)
+    {
+    Console.Write("суббота");
+    }
+    else
+    {
+      Console.Write("воскресенье");  
+    }
+    break;
+}
