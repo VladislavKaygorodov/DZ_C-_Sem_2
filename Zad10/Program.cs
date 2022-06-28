1d5fb821e2a4258b84f016413200866e40ebd1be
Vladislav <kaygorodov.vlad@gmail.com>
2022-06-28 20:58:01 +0700

    Решил задачу 10

---
+++ Zad10/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = new Random().Next(100,1000);
+Console.WriteLine($"Рандомное число: {number}");
+
+int NumOne = number / 100;
+int NumTwo = number % 10;
+int FinfNum = ( number - (NumOne * 100 + NumTwo)) / 10;
+
+Console.WriteLine($"Вторым числом заданного значения является: {FinfNum}");
